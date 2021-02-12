e6bb7753f6c6ea21410eb6e4d325813d5ffaf21d
Radim Novotny <novotny.radim@gmail.com>
2021-02-12 10:55:53 +0100

    Use ffmpeg to capture the frames

--- capture.py
+++ capture.py
@@ -1,29 +1,24 @@
 import time
-import vlc
+import subprocess
 import datetime
 import os
 import timelapseconfig
-
-#prepare VLC
-i = vlc.Instance("--vout=dummy") # when running from cron, there is no screen attached
-camera = i.media_player_new()
-camera.set_mrl(timelapseconfig.rtsp_url)
-camera.audio_set_volume(0)
-camera.play()
 
 # prepare the output folder
 write_path = timelapseconfig.output_dir + datetime.datetime.today().strftime(timelapseconfig.daily_foldername_date_formatstring)
 if not os.path.exists(write_path):
     os.makedirs(write_path)
 
-print ('waiting for VLC')
-time.sleep(10)
 print ("staring capture")
 starttime = datetime.datetime.now().timestamp()
 while datetime.datetime.now().timestamp() < starttime + timelapseconfig.cronjob_repeat_time:
     cycle_starttime = datetime.datetime.now().timestamp()
     file_name = write_path + "/" + str(int(datetime.datetime.now().timestamp())) + ".png"
     print(file_name)
-    camera.video_take_snapshot(0,file_name,1920,1080)
+    cmd = [timelapseconfig.ffmpeg] + timelapseconfig.ffmpeg_cmdline + [file_name]
+    result = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if result.returncode != 0:
+        print('Command:', ' '.join(cmd))
+        print('Unable to take a capture. Returncode:', result.returncode)
     while datetime.datetime.now().timestamp() < cycle_starttime + timelapseconfig.delay_between_images:
         time.sleep(0.1)
