2a5f8ee4cab8988332daa87f444bc1a7c1810665
Radim Novotny <novotny.radim@gmail.com>
2021-02-26 08:09:13 +0100

    sort files before deletion so we really keep every 10th image in sequence

--- makevideo.py
+++ makevideo.py
@@ -8,11 +8,11 @@ yesterdays_folder = timelapseconfig.output_dir + yesterdays_date
 filename = yesterdays_date + ".mp4"
 output_video_path = timelapseconfig.output_dir_for_daily_timelapse_videos + filename
 os.chdir(yesterdays_folder)
-returnval = os.system("ffmpeg -r 24 -pattern_type glob -i '*.png' -s hd1080 -vcodec libx264 '" + output_video_path + "'")
+returnval = os.system("ffmpeg -r 24 -pattern_type glob -i '*.png' -s hd1080 -vcodec libx264 -g 8 '" + output_video_path + "'")
 n = str(int(timelapseconfig.keep_every_nth_picture))
 
 if returnval == 0: #if making the video was successful, delete the files
-    os.system("for file in `find " + yesterdays_folder + " -type f | awk 'NR %" + n + " != 0'`; do rm $file ; done")
+    os.system("for file in `find " + yesterdays_folder + " -type f | sort | awk 'NR %" + n + " != 0'`; do rm $file ; done")
 
 if timelapseconfig.symlink_latest_daily_dirname:
     if not os.path.isdir(timelapseconfig.symlink_latest_daily_dirname):
